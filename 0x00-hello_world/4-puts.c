4ec8acaada1f4a1bf5ae96e5815ad561113609eb
aidan0802 <aidanheydenrycht@gmail.com>
2023-06-15 04:47:31 -0700

    added betty doc for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * prints out statement
+ *
+ * return: Always (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

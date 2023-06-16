2f05548a52d3208b7946cfc9f4c3c505ba8b4372
aidan <aidanheydenrycht@gmail.com>
2023-06-17 01:01:56 +0300

    5-print_numbers.c file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

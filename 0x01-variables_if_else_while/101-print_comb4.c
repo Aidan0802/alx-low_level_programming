3a15c761958f5d454254a4fdf0b7f4011431b4f8
aidan <aidanheydenrycht@gmail.com>
2023-06-18 14:54:44 +0300

    101-print_comb4.c fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	int a3;
 	int a2;
@@ -13,11 +13,11 @@ int main(void)
 	int c1 = 0;
 	int c2 = 0;
 
-	for (a3 = 48; a3 < 56;a3++)
+	for (a3 = 48; a3 < 56; a3++)
 	{
 		a1 = 49;
 		a1 += c1;
-		
+
 		for (; a1 < 58; a1++)
 		{
 			a2 = 50;
@@ -25,7 +25,7 @@ int main(void)
 
 			for (; a2 < 58; a2++)
 			{
-				if (a1 != a2 && a1 < a2 )
+				if (a1 != a2 && a1 < a2)
 				{
 					putchar(a3);
 					putchar(a1);

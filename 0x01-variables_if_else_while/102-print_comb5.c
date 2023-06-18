dc80108e8a1321462c20f0187e30af634b58a0e9
aidan <aidanheydenrycht@gmail.com>
2023-06-18 21:01:40 +0300

    102-print_comb5.c fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,44 +8,43 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
-	int brk = 0;
+int a, b, c, d;
+int brk = 0;
 
-	for (a = 48; a < 58; a++)
+for (a = 48; a < 58; a++)
+{
+	for (b = 48; b < 58; b++)
 	{
-		for (b = 48; b < 58; b++)
+		for (c = 48; c < 58; c++)
 		{
-			for (c = 48; c < 58; c++)
+			if (brk == 1)
 			{
-				if (brk == 1)
-				{
-					break;
-				}
-				for (d = 48; d < 58; d++)
-				{
-					if (b < d)
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
+				break;
+			}
+			for (d = 48; d < 58; d++)
+			{
+				int i = (a * 10) + c;
+				int x = (b * 10) + d;
 
-						if (a == '9' && b == '8' && c == '9' && d == '9')
-						{
-							brk = 1;
-							break;
-						}
-						putchar(',');
-						putchar(' ');
+				if (b < d)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a == '9' && b == '8' && c == '9' && d == '9')
+					{
+						brk = 1;
+						break;
 					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }

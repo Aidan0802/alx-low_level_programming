21325dcea7d9901667fc6cd8bb14f698a72e03c7
aidan <aidanheydenrycht@gmail.com>
2023-06-18 11:42:02 +0300

    100-print_comb3.c file added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a ;
+	int b ;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++;)
+		{
+			if ( a1 != a2 && a1 < a2)
+			{
+				putchar(a1);
+				putchar(a2);
+				if (a1 == 56 && a2 == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}

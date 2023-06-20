09486fe2849510f54e5bac84a26ead0d3a32e004
aidan <aidanheydenrycht@gmail.com>
2023-06-20 15:43:21 +0300

    added files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints out _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char a[8] = {'_','p','u','t','c','h','a','r'};
+	int i;
+
+	for (i = 0; i < 7; i++)
+	{
+	    _putchar(a[i]);
+	}
+	_putchar('\n');
+	    return 0;
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+
+int _putchar(char c);
+
+void print_alphabet(void)
+{
+	char a = 97;
+
+	while (a < 123)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}

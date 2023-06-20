a6b66074c5eb898feb7712f0696d55320b9299e3
aidan <aidanheydenrycht@gmail.com>
2023-06-20 19:59:26 +0300

    1-alphabet.c fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - Printing the alphabet
+ * print_alphabet - Printing the alphabet
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
-
 void print_alphabet(void)
 {
 	int a = 97;
-	
+
 	while (a < 123)
 	{
 		_putchar(a);
 		a++;
 	}
+	_putchar('\n');
 }

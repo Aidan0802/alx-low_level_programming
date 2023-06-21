1178adf6eb62d74bbdca2c5006758f64782ba5dc
aidan <aidanheydenrycht@gmail.com>
2023-06-21 21:34:25 +0300

    5-sign.c fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * _prints_sign - checks n and prints sign
  *
- * @c: - if n > 0 then print +, is 0 print 0
+ * n: - if n > 0 then print +, is 0 print 0
  * if < 0 print -
  *
  * Return: 1 if positive, 0 if 0 and -1 if negative
@@ -11,12 +11,12 @@
 
 int _print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);

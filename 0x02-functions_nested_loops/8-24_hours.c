8583bdb7d9b055dcd0677fa7eae8967b8df6f859
aidan <aidanheydenrycht@gmail.com>
2023-06-22 01:46:22 +0300

    added 8 and updated main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	for (h1 = 48; h1 < 51; h1++)
+	{
+		for (h2 = 48; h2 < 52; h2++)
+		{
+			for (m1 = 48; m1 < 53; m1++)
+			{
+				for (m2	= 48; m2 < 58; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
+
+}

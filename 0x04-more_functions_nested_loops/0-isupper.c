b3ecba4dc5dc4ffd82ada5809d7a195c06d1900a
aidan <aidanheydenrycht@gmail.com>
2023-06-22 12:52:24 +0300

    added new files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the char is an upper
+ *
+ * @c: intakes a char
+ *
+ * Return: 1 if its an upper, 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	int a = 65;
+
+	while (a < 91)
+	{
+		if (c = a)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}

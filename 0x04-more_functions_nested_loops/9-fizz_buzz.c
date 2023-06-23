43c0fa7d840ae97f9d6b7259b386d2bb85267c25
aidan <aidanheydenrycht@gmail.com>
2023-06-23 22:05:09 +0300

    fixed bug

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (i == 1)
+			printf("%i", i);
+
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
-		}
+
 		else if (i % 3 == 0)
-		{
 			printf(" Fizz");
-		}
 
 		else if (i % 5 == 0)
-		{
 			printf(" Buzz");
-		}
 		else
-		{
 			printf(" %i", i);
-		}
 	}
 	printf("\n");
 	return (0);

14c3d298399e420f31d433adba296438db2aa26d
aidan <aidanheydenrycht@gmail.com>
2023-06-23 13:48:46 +0300

    updated main.h added 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints a # triangle
+ *
+ * @size: angle size
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size; j > i; j++)
+		{
+			_putchar(' ');
+		}
+		
+		for (k = 1; k < i + 1; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}

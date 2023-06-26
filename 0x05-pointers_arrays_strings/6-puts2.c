526ccc691ebe6507f3f0efdd1800489c0311572a
aidan <aidanheydenrycht@gmail.com>
2023-06-26 19:05:03 +0300

    fixed bugs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,14 +3,22 @@
 /**
  * puts2 - prints every other char of a string
  *
+ * @str: string input
+ *
  * Return: void
  */
 
 void puts2(char *str)
 {
-	int i;
-	
-	for (i = 0; str[i]; i += 2)
+	int i, l;
+	int len = 0;
+
+	for (l = 0; str[l]; l++)
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}

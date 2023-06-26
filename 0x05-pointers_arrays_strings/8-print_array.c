17dd55870dfc09b323412262b64557592cb28882
aidan <aidanheydenrycht@gmail.com>
2023-06-26 21:09:06 +0300

    fixed bugs

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%i\n", a[i]);
-		}
-		else
-		{
-			printf("%i, ", a[i]);
-		}
+		printf("%c", a[i]);
+		if (n != (n - 1))
+			printf(", ");
 	}
+	putchar('\n');
 }

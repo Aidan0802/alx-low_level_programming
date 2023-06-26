b7ea47358036fb1903b8356734ede642fbe62f61
aidan <aidanheydenrycht@gmail.com>
2023-06-26 18:47:04 +0300

    updated main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 void _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-void _strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);

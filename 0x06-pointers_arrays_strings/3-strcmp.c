6525c34751ae47ee347295a28d6b209338a39049
aidan <aidanheydenrycht@gmail.com>
2023-06-30 21:28:16 +0300

    fixed bug

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,25 +13,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, count = 0;
+	int i, match = 0;
 
-	if (*s1 == *s2)
-		return (0);
-	else if (s1[0] > s2[0])
+	for (i = 0; match = 0; i++)
 	{
-
-		for (i = s1[0]; i > s2[0]; i--)
-		{
-			count++;
-		}
-		return (count);
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		match = s1[i] - s2[i];
 	}
-	else
-	{
-		for (i = s1[0]; i < s2[0]; i++)
-		{
-			count--;
-		}
-		return (count);
-	}
+	return (match);
 }

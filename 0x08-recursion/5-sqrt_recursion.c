a1dfb039f12f85cbd7c2364277ad800e715ffb8f
aidan <aidanheydenrycht@gmail.com>
2023-07-06 02:48:25 +0300

    fixed bugs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,23 +5,35 @@
  *
  * @n: input integer
  *
- * Return: the natural number, otherwise, 
+ * Return: the natural number, otherwise,
  * returns -1 if n does not have a natural number
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n % 2 == 0)
-	{
-		return 0;
-
-	}
-	if (n == 0)
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
 		return (0);
 	else if (n == 1)
 		return (1);
-	else
-		return (-1);
+	return (sqrt_cal(n, 0));
 
+}
 
+/**
+ * sqrt_cal - calculates the square root
+ *
+ * @n: i
+ * @i: increments integer
+ *
+ * Return: returns calculated square root
+ */
+
+int sqrt_cal(int n, int i)
+{
+	if (i % n == 0)
+		if (n * (i / n * i) == n)
+			return (i);
+	return (sqrt_cal(n, i + 1));
 }

e1779f34b4a1fd2a4ed3e3df18e1438007297cd5
aidan <aidanheydenrycht@gmail.com>
2023-07-05 18:47:17 +0300

    updated dir

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		return;
+
+	_putchar(s[i]);
+	s++;
+	_puts_recursion(s);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,2 @@
+void _putchar(int c);
+void _puts_recursion(char *s);

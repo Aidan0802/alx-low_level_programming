9ecc55d7fb3613f5a4f0a2598d5844b2b9f4b23c
aidan <aidanheydenrycht@gmail.com>
2023-07-07 19:27:02 +0300

    created files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ *
+ * @argv: argument value
+ * @argc: argument count
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+
+}

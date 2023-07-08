fb60260702c6e34ce806734b008629d93f6969dd
aidan <aidanheydenrycht@gmail.com>
2023-07-08 09:34:08 +0300

    fixed bugs

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
@@ -11,34 +12,18 @@
 
 int main(int argc, char *argv[])
 {
-
-	char *n1 = argv[1];
-	char *n2 = argv[2];
-
-	int i = 0, x = 0, num1[1] = {0}, num2[1] = {0};
-	int sign = 1;
-
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	if (n1[1] == '-' || n2[1] == '-')
-		sign *= -1;
-
-	while (n1[i] != '\0')
+	else
 	{
-		num1[i] = num1[i] * 10 + (n1[i] - 48);
-		i++;
-	}
+		int n1 = atoi(argv[1]);
+		int n2 = atoi(argv[2]);
 
-	while (n2[x] != '\0')
-	{
-		num2[x] = num2[x] * 10 + (n2[x] - 48);
-		x++;
+		printf("%i\n", n1 * n2);
 	}
-	printf("%i\n", num1[0] * num2[0]);
 
 	return (0);
 

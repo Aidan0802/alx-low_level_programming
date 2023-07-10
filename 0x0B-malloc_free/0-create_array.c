65693bfb1399e75add74e1e29d9caa0f2dc3cb3d
aidan <aidanheydenrycht@gmail.com>
2023-07-10 14:51:04 +0300

    fixed malloc size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *buffer = malloc(size * sizeof(unsigned int));
+	char *buffer = malloc(size);
 
 	if (size == 0)
 		return ('\0');

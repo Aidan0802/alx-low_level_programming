2d5e5659cec0815fffd4542d037b44f22203fe0e
aidan <aidanheydenrycht@gmail.com>
2023-07-10 14:31:34 +0300

    created files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates a array of chars
+ *
+ * @size: unsigned integer
+ * @c: input character
+ *
+ * Return: returns 0 if size is 0
+ * otherwise returns pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return ('\0');
+	else
+	{
+		unsigned int i;
+		char *buffer = malloc(size * sizeof(unsigned int));
+
+		for (i = 0; i < size; i++)
+		{
+			buffer[i] = c;
+		}
+
+		return (buffer);
+	}
+}

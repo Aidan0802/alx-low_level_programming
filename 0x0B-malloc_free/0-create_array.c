0ee1e4fcdcebb6e623e96782279c3c8cc64812f6
aidan <aidanheydenrycht@gmail.com>
2023-07-10 15:08:41 +0300

    handle malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *buffer = malloc(size);
 
-	if (size == 0)
+	if ((sizeof(buffer)) == '\0' || size == 0)
 		return ('\0');
 
 	for (i = 0; i < size; i++)

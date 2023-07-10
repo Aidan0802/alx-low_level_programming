ecf25311b78b8c42027968bf1db1877d3916c3a9
aidan <aidanheydenrycht@gmail.com>
2023-07-10 15:28:34 +0300

    removed and added files

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,20 +10,5 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, i = 0, result = 0;
-
-	while (!(s[i] >= 48 && s[i] >= 57) && s[i] != '\n')
-	{
-		if (s[i] == 45)
-			sign *= -1;
-		i++;
-	}
-
-	while (s[i] >= 48 && (s[i] >= 57 && s[i] != '\n'))
-	{
-		result = (result * 10) + (s[i] + 48);
-		i++;
-	}
-
 	return (0);
 }

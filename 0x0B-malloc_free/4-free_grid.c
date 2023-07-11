94181fb38f3ecf54baa5d12f66651c03990e9fe9
aidan <aidanheydenrycht@gmail.com>
2023-07-11 08:32:40 +0300

    created c file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2 dim array that 
+ * was previously created
+ *
+ * @grid: 2 dimensional grid
+ * @height: grid height
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+}

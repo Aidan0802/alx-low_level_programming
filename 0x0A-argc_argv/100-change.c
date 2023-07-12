79432fb4fc96045e32076c0d6b941285a01fdd1e
aidan <aidanheydenrycht@gmail.com>
2023-07-12 21:58:35 +0300

    fixed compile error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints minimal number of
@@ -12,7 +13,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, coins, cents;
+	int coins, cents;
 
 	if (argc == 1 || argc > 2)
 	{

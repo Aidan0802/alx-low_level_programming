7b9d3e9cf7652823165cac40f2f48814827b3670
aidan <aidanheydenrycht@gmail.com>
2023-07-13 21:04:40 +0300

    fixed bugs

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,17 +14,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char **arr;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * sizeof(char));
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		arr[i] = malloc(size * sizeof(char));
+	for (i = 0; i < sizei * nmemb; i++)
+		arr[i] = 0;
 
 	return (arr);
 }

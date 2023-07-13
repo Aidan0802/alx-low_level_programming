c98b803baa0b2ae69519af39f9ccd256ffaeb042
aidan <aidanheydenrycht@gmail.com>
2023-07-13 23:40:10 +0300

    fixed betty style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(max + 1);
+	arr = malloc(sizeof(*arr) * max + 1 - min);
 	if (arr == NULL)
 		return (NULL);
 

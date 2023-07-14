3ecb74bc845b5d5bbf164e55c76c70d838632d84
aidan <aidanheydenrycht@gmail.com>
2023-07-14 11:44:05 +0300

    fixed loop error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 
-	for (x = 0 ; x <= n; i++, x++)
+	for (x = 0 ; x < n; i++, x++)
 		str[i] = s2[x];
 
 	return (str);

ae2b8c35744b430b1a7f2b9fb0545852aa57d3a5
aidan <aidanheydenrycht@gmail.com>
2023-07-14 12:19:16 +0300

    fixed valgrind error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(*arr) * max + 1 - min);
+	arr = malloc(sizeof(*arr) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 

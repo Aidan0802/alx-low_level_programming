7aaff18a516a646590468dd4a04ebc6cb10394ac
aidan <aidanheydenrycht@gmail.com>
2023-07-14 11:58:09 +0300

    fixed uninitialised heap values

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,6 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(len1 + len2 + 1);
 	if (str == NULL)
 		return (NULL);
+	for (i = 0; i < len1 + len2 + 1; i++)
+		str[i] = 0;
 
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];

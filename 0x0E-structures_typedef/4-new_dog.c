7e31543a6be508b8a2fc0ceb62fbf333ef15e513
aidan <aidanheydenrycht@gmail.com>
2023-07-17 22:04:52 +0300

    added if statements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,6 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i, n_len = 0, o_len = 0;
 	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	for (i = 0; name[i]; i++)
 		n_len++;
 

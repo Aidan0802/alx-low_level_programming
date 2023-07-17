a4dbbd30885e6b4d0158e2234df4a270c1ca66f5
aidan <aidanheydenrycht@gmail.com>
2023-07-17 09:02:41 +0300

    created header and c files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints file name it was compiled from
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

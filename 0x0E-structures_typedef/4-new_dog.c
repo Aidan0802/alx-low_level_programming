71ccf9b9712ea26dbaccade42b589fe88cbcaa21
aidan <aidanheydenrycht@gmail.com>
2023-07-17 15:54:23 +0300

    removed malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,18 +14,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, n_len = 0, o_len = 0;
 
-	for (i = 0; name[i]; i++)
-		n_len++;
-	for (i = 0; owner[i]; i++)
-		owner++;
+	dog = malloc(sizeof(dog_t));
 
-	dog->age = malloc(sizeof(dog_t) * sizeof(int));
-	dog->name = malloc(sizeof(dog_t) * n_len);
-	dog->owner = malloc(sizeof(dog_t) * o_len);
-
-	if (dog == NULL || dog->name == NULL || dog->owner == NULL)
+	if (dog == NULL)
 		return (NULL);
 
 	dog->name = name;

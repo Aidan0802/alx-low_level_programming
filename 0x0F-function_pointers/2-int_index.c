f039d006b95a8eeaaf16a3c646686abdf5322d63
aidan <aidanheydenrycht@gmail.com>
2023-07-19 13:27:56 +0300

    removed white spaces added if statement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,19 +16,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res;
 
-	if (array == NULL)
-		return (NULL);
-
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		res = cmp(array[i]);
+		if (size <= 0)
+			return (-1);
 
-		if (res)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			res = cmp(array[i]);
+
+			if (res)
+				return (i);
+		}
 	}
-
 	return (-1);
 }

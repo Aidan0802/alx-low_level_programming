2f67afd6f3be0b2a12e763b95e15d68efa0ac3a0
aidan <aidanheydenrycht@gmail.com>
2023-07-20 08:15:57 +0300

    fix if statements

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,15 +19,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
-
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
 		printf("%i", x);
 
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");

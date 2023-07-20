37d7836ccb6a1a80d3571f93a237fe7a55589825
aidan <aidanheydenrycht@gmail.com>
2023-07-20 05:52:36 +0300

    updated header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 int _putchar(int c);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif

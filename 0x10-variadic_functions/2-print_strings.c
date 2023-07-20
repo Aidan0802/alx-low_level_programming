228acb7edac822fb18ed2fd15302fd00a06ccc0c
aidan <aidanheydenrycht@gmail.com>
2023-07-20 08:06:04 +0300

    fix if statements

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,18 +14,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 	char *s;
-	
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
+
 		if (s == NULL)
 			printf("(nil)");
 		else
 			printf("%s", s);
-		if (separator != NULL)
-			if (n - 1 > i)
-				printf("%s ", separator);
+
+		if (n - 1 > i)
+			if (separator != NULL)
+					printf("%s ", separator);
 	}
 	printf("\n");
+	va_end(args);
 }

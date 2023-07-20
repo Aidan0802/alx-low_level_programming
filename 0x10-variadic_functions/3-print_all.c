6415ed74c7c29652fde5911d8c8ddbeca87b502b
aidan <aidanheydenrycht@gmail.com>
2023-07-20 07:56:37 +0300

    added variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, n = 0;
+	int i = 0, n = 0, l = 0;
 	va_list args;
 	const char format_args[] = "cifs";
 	char *s;
@@ -22,16 +22,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%c", va_arg(args, int), l = 1);
 				break;
 			case 'i':
-				printf("%i", va_arg(args, int));
+				printf("%i", va_arg(args, int), l = 1);
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%f", va_arg(args, double), l = 1);
 				break;
 			case 's':
-				s = va_arg(args, char *);
+				s = va_arg(args, char *), l = 1;
 				if (s == NULL)
 				{
 					printf("(nil)");
@@ -42,7 +42,7 @@ void print_all(const char * const format, ...)
 		}
 		while (format_args[n])
 		{
-			if (format[i] == format_args[n] && format[i + 1] != '\0')
+			if (format[i] == format_args[n] && format[i + 1] != '\0' && l)
 			{
 				printf(", ");
 				break;

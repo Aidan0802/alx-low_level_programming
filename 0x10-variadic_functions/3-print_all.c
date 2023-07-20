324b8836941e159001c2af7bc34e4fdf0af57e85
aidan <aidanheydenrycht@gmail.com>
2023-07-20 08:01:19 +0300

    removed variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, n = 0, l = 0;
+	int i = 0, n = 0;
 	va_list args;
 	const char format_args[] = "cifs";
 	char *s;
@@ -22,16 +22,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int)), l = 1;
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				printf("%i", va_arg(args, int)), l = 1;
+				printf("%i", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double)), l = 1;
+				printf("%f", va_arg(args, double));
 				break;
 			case 's':
-				s = va_arg(args, char *), l = 1;
+				s = va_arg(args, char *);
 				if (s == NULL)
 				{
 					printf("(nil)");
@@ -42,7 +42,7 @@ void print_all(const char * const format, ...)
 		}
 		while (format_args[n])
 		{
-			if (format[i] == format_args[n] && format[i + 1] != '\0' && l)
+			if (format[i] == format_args[n] && format[i + 1] != '\0')
 			{
 				printf(", ");
 				break;

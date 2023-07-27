2452921c8d0746ec5efeba1e809966602895edaf
aidan <aidanheydenrycht@gmail.com>
2023-07-27 20:25:05 +0300

    [Created] C function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - prints before main
+ *
+ * Return: void
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

8b02b522566423677b56158cc557a8bb8a559b2f
aidan <aidanheydenrycht@gmail.com>
2023-07-27 21:02:03 +0300

    [Created] asm file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+	SECTOIN .data
+msg:	db "Hello, Hilberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION	.text
+	extern printf
+	global main, _start
+main:
+_start:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov ebx, 0
+	ret

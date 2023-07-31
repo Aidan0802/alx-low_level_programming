a90c28043332daacd15243c2942c2a5d6693b4c9
aidan <aidanheydenrycht@gmail.com>
2023-07-31 11:23:39 +0300

    created files

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ *
+ * @h: address that points to first node
+ * Return: The number of list. Otherwise -1 if NULL
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

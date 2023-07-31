7f2439159a4de727d092268c28ad5e4d79a14e88
aidan <aidanheydenrycht@gmail.com>
2023-07-31 13:43:01 +0300

    [Created] c function file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free listint_t list and set head
+ * to NULL
+ *
+ * @head: node address
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}

0c9d2ce73cbb86a2b110a20f9b98d153126cbd86
aidan <aidanheydenrycht@gmail.com>
2023-08-02 17:12:11 +0300

    [Fix] return value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	newNode->n = n;
-	if (idx == 0 || (idx - 1) == 0)
+	if (idx == 0)
 	{
 		newNode->next = insert;
 		*head = newNode;

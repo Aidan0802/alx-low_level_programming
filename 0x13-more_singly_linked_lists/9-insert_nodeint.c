d2ebf6534802f251ee37c6cf01be120b6b72f195
aidan <aidanheydenrycht@gmail.com>
2023-08-02 17:17:15 +0300

    [Fix] return value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -50,5 +50,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = insert->next;
 		insert->next = newNode;
 	}
-	return (*head);
+	return (newNode);
 }

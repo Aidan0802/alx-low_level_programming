00ff250fbc4d49df010025f9b6b81554282a24db
aidan <aidanheydenrycht@gmail.com>
2023-08-02 17:24:31 +0300

    [Fix] compilation error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *selected = *head;
 	listint_t *del;
 
-	if (!head || (!head && index != 0)
+	if (!head || (!head && index != 0))
 		return (-1);
 
 	if (index == 0)

aad04ea8e3da07947d64d524554e9ec452ff326c
aidan <aidanheydenrycht@gmail.com>
2023-08-03 20:27:11 +0300

    [Created] c file function

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets bit at given index
+ *
+ * @n: address of value
+ * @index: index to set
+ * Return: 1 (Succesful) 0 (Fail)
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | 1 << index;
+
+	if (*n)
+		return (1);
+	else
+		return (0);
+}

322556aa88159d4e9dd48219e68ddbbf420f196a
aidan <aidanheydenrycht@gmail.com>
2023-08-03 20:46:44 +0300

    [Created] c file function

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets bit value at given index to 0
+ *
+ * @n: address of input
+ * @index: Of bit that needs to be set
+ * Return: 1 (Success). 0 (Fail)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1 << index);
+
+	return (1);
+}

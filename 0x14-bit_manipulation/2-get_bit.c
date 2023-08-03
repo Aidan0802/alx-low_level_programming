58d4c117a34197694b939b7786557e5da15e9810
aidan <aidanheydenrycht@gmail.com>
2023-08-03 19:16:58 +0300

    [Created] c file function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - returns bit of given index
+ *
+ * @n: unsigned integer
+ * @index: bit value to get
+ * Return: Returns bit value at index
+ * otherwise -1 if error occurs
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit;
+
+	bit = (n >> index) & 1;
+
+	return (bit);
+}

5c7abb8ec87d5b0c5b4c57377c8b78f7882f5ca6
aidan <aidanheydenrycht@gmail.com>
2023-08-07 23:58:23 +0300

    [Fix] NULL content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,10 +21,14 @@ int create_file(const char *filename, char *text_content)
 	f_input = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
 	if (f_input == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
 
-	if (len != 0)
-		write(f_input, text_content, len);
+		if (len != 0)
+			write(f_input, text_content, len);
+	}
+	close(f_input);
 	return (1);
 }

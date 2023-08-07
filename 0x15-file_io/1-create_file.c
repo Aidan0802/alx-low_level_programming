ff8fc4b37a1179a60c7958e136227a741a5c35d4
aidan <aidanheydenrycht@gmail.com>
2023-08-07 21:03:07 +0300

    [Fix] if statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	f_input = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
-	if (!f_input)
+	if (f_input == -1)
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
 
 	if (len != 0)
-	{
 		write(f_input, text_content, len);
-	}
 	return (1);
 }

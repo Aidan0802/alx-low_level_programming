d8974bb401e5b2cc95f27fe533449d604441dfd6
aidan <aidanheydenrycht@gmail.com>
2023-08-07 17:58:36 +0300

    [Created] files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads text and prints to standart output
+ *
+ * @filename: File to be read
+ * @letters: Number of letter to read and print
+ * Return: Number of letters that was read and printed
+ * 0 if it cannot be opened or read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char c;
+	size_t i = 0;
+
+	if (!filename)
+		return (0);
+
+	fp = fopen(filename, "r");
+	if (!fp)
+		return (0);
+	
+	while (c != EOF)
+	{
+		if (i == letters)
+			break;
+		c = fgetc(fp);
+		_putchar(c);
+		i++;
+	}
+	fclose(fp);
+
+	return (i);
+}

b0f7f370cbedc56d4b9f3161e78f8709ac87d6a8
aidan <aidanheydenrycht@gmail.com>
2023-08-11 14:13:58 +0300

    [Update] Header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,25 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define MAX_BYTES 1024
+
+/**
+ * struct elf_head - Structure of elf head
+ * @ident: Array that holds identification number 
+ * @type: Type of file
+ * @version: The version of the file
+ * @entry: The entry point address
+ */ 
+
+struct h_info
+{
+	uint8_t  ident[16];
+	uint16_t type;
+	uint32_t version;
+	uint64_t entry;
+};
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);

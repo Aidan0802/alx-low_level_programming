5b18e3882ac13482b8027dee5ef28a7cb4f771cc
aidan <aidanheydenrycht@gmail.com>
2023-08-11 11:32:40 +0300

    [Fix] betty-style, return values

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int f_from, f_to, size, f_i, f_o;
+	int f_from, f_to, size, f_i, f_o, fd_val;
 	char buf[MAX_BYTES];
 
 	if (argc != 3)
@@ -18,30 +18,35 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	if (access(argv[1], 0) != 0 || (f_from = open(argv[1], O_RDONLY)) == -1)
+	if (access(argv[1], 0) != 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 	if (access(argv[2], 0) == 0)
 		f_to = open(argv[2], O_TRUNC | O_WRONLY);
 	else
 		f_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	if ((f_to = open(argv[2], O_WRONLY | O_CREAT, 0664)) == -1)
+	if (f_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((size = read(f_from, buf, MAX_BYTES)) > 0)
 		write(f_to, buf, size);
-
-	f_i = close(f_from);
-	f_o = close(f_to);
-
-	if (f_i != 0 && f_o != 0)
+	f_i = close(f_from), f_o = close(f_to);
+	if (f_i != 0 || f_o != 0)
+	{
+		fd_val = (f_i != 0) ? f_from : f_to;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_val);
 		exit(100);
-
+	}
 	return (0);
 }

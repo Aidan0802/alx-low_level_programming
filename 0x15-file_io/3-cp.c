fa0f303368f5425e0d1a86441baae1a3d0350a14
aidan <aidanheydenrycht@gmail.com>
2023-08-11 12:05:45 +0300

    [Fix] betty-style, return values

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Copies content from one file to another
  *
@@ -10,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int f_from, f_to, size, f_i, f_o, fd_val;
+	int f_from, f_to, size, f_i, f_o, fd_val, wr_val;
 	char buf[MAX_BYTES];
 
 	if (argc != 3)
@@ -28,11 +27,6 @@ int main(int argc, char *argv[])
 		f_to = open(argv[2], O_TRUNC | O_WRONLY);
 	else
 		f_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	if (f_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 	while (size > 0)
 	{
 		size = read(f_from, buf, MAX_BYTES);
@@ -41,7 +35,12 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		write(f_to, buf, size);
+		wr_val = write(f_to, buf, size);
+		if (wr_val == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	f_i = close(f_from), f_o = close(f_to);
 	if (f_i != 0 || f_o != 0)
@@ -49,6 +48,5 @@ int main(int argc, char *argv[])
 		fd_val = (f_i != 0) ? f_from : f_to;
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_val);
 		exit(100);
-	}
-	return (0);
+	} return (0);
 }

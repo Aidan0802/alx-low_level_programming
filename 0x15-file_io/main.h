89e53b233d2911825bdfd4ffa30876ff1f8258ac
aidan <aidanheydenrycht@gmail.com>
2023-08-25 15:54:45 +0300

    [Update] Header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,33 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
 
-#define MAX_BYTES 1024
-
-/**
- * struct h_info - Structure of elf head
- * @ident: Array that holds identification number
- * @type: Type of file
- * @version: The version of the file
- * @entry: The entry point address
- */
-
-struct h_info
-{
-	uint8_t  ident[16];
-	uint16_t type;
-	uint32_t version;
-	uint64_t entry;
-};
+#define ELF_HEADER_SIZE 0x20
+#define CLASS_OFFSET 0x04
+#define DATA_OFFSET 0x05
+#define VERSION_OFFSET 0x06
+#define OS_ABI_OFFSET 0x07
+#define ABI_VERSION_OFFSET 0x08
+#define TY_OFFS 0x10
+#define ENTRY_POINT_OFFSET 0x18
+#define ERROR_NOT_ELF 98
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void display_elf(int);
+void print_error(const char *);
+void read_header(int, char elf_header[ELF_HEADER_SIZE]);
 
 #endif

0cf3867b180c5080b6d98dd0c5201e69a15feb70
aidan <aidanheydenrycht@gmail.com>
2023-08-25 15:55:08 +0300

    [Modified] entire code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,46 +1,107 @@
 #include "main.h"
 
 /**
- * main - Displays info contained in ELF header
+ * main - Read elf header
  * @argc: Argument count
  * @argv: Argument vector
- * Return: Always 0 (Success)
+ * Return: 0 Always (Succes)
  */
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	int i;
-	struct h_info info_p;
+	int fd;
 
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-		return (1);
+		print_error("Usage: elf_header elf_filename");
 	}
 
-	fp = fopen(argv[1], "rb");
-	if (!fp)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
-		perror("File cannot open\n");
-		exit(98);
+		print_error("Unable to open the file");
 	}
 
-	fread(&info_p, sizeof(info_p), 1, fp);
-	fclose(fp);
+	display_elf(fd);
 
-	printf("ELF Header:\n");
-	printf("  Magic:  ");
-	for (i = 0; i < 16; i++)
-		printf("%02x ", info_p.ident[i]);
-	putchar('\n');
+	close(fd);
 
-	printf("  Class:			     %d\n", info_p.ident[4]);
-	printf("  Data:				     %d\n", info_p.ident[5]);
-	printf("  Version:			     %u (current)\n", info_p.ident[6]);
-	printf("  OS/ABI:			     %d\n", info_p.ident[7]);
-	printf("  ABI Version:			     %u\n", info_p.ident[8]);
-	printf("  Type:				     %u (Executable file)\n", info_p.type);
-	printf("  Entry point address:		     0x%lx\n", info_p.entry);
 	return (0);
+}
+/**
+ * display_elf - Display elf header
+ * @fd: File discription
+ * Return: void
+ */
+void display_elf(int fd)
+{
+	char elf_header[ELF_HEADER_SIZE];
+	int i;
+	uint8_t abi_version;
+	uint64_t entry_point;
+
+	const char *class_names[] = {"None", "ELF32", "ELF64"};
+	const char *data_names[] = {"None", "2's complement, little endian",
+		"2's complement, big endian"};
+	const char *os_abi_names[] = {
+	"UNIX - System V", "UNIX - SHP-UX", "UNIX - SNetBSD", "UNIX - SLinux",
+	"UNIX - SGNU Hurd", "UNIX - SSolaris", "UNIX - SAIX", "UNIX - SIRIX",
+	"UNIX - SFreeBSD", "UNIX - STru64", "UNIX - SNovell Modesto",
+	"UNIX - SOpenBSD", "UNIX - SOpenVMS", "UNIX - SNonStop Kernel",
+	"UNIX - SAROS", "UNIX - SFenix OS", "UNIX - SCloudABI",
+	};
+	const char *type_names[] = {
+	"None", "REL (Relocatable)", "EXEC (Executable)", "DYN (Shared object)",
+	"CORE (Core)",
+	};
+
+	read_header(fd, elf_header);
+
+	abi_version = (uint8_t)elf_header[ABI_VERSION_OFFSET];
+	entry_point = *(uint64_t *)(elf_header + ENTRY_POINT_OFFSET);
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < 16; ++i)
+	{
+		printf("%02x ", (unsigned char)elf_header[i]);
+	}
+	printf("\n");
+	printf("  Class: %*s\n", 33, class_names[(int)elf_header[CLASS_OFFSET]]);
+	printf("  Data: %*s\n", 58, data_names[(int)elf_header[DATA_OFFSET]]);
+	printf("  Version: %*d (current)\n", 27, (int)elf_header[VERSION_OFFSET]);
+	printf("  OS/ABI: %*s\n", 42, os_abi_names[(int)elf_header[OS_ABI_OFFSET]]);
+	printf("  ABI Version: %*d\n", 23, (int)abi_version);
+	printf("  Type: %*s\n", 48, type_names[*(uint16_t *)(elf_header + TY_OFFS)]);
+	printf("  Entry point address: %#*lx\n", 20, (unsigned long int)entry_point);
+}
+/**
+ * read_elf - Read elf header
+ * @fd: File descriptor
+ * @elf_header: Elf header
+ * Return: void
+ */
+
+void read_header(int fd, char elf_header[ELF_HEADER_SIZE])
+{
+	if (lseek(fd, 0, SEEK_SET) == -1)
+	{
+		print_error("Unable to seek to file start");
+	return;
+	}
+
+	if (read(fd, elf_header, ELF_HEADER_SIZE) != ELF_HEADER_SIZE)
+	{
+		print_error("Unable to read ELF header");
+	}
+}
+/**
+ * print_error - Prints error message
+ * @message: Message to be printed
+ * Return: void
+ */
+
+void print_error(const char *message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(ERROR_NOT_ELF);
 }

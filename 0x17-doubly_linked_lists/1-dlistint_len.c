200da76848694ff48276a11a448dd4ee4453118e
aidan <aidanheydenrycht@gmail.com>
2023-09-14 23:20:29 +0300

    [Created] C file function

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Counts number of elements of list
+ * @h: Address of the node
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}

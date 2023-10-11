60c17d73a9d600258bbe73bcab2712f0e6b97a73
aidan <aidanheydenrycht@gmail.com>
2023-10-11 19:47:18 +0200

    [Fix] compile error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new_table = malloc(sizeof(has_table_t));
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(has_table_t *) * size);
+	new_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);

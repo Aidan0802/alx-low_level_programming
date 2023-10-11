023fc3a0b9ee9ffbe09d441f5f12db19a2c36616
aidan <aidanheydenrycht@gmail.com>
2023-10-11 20:12:24 +0200

    [Fix] Documentation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t *hash_table_create - Creates a hash table.
+ * hash_table_create - Creates a hash table.
  * @size: The size of the array
  * Return: Pointer to the newly created hash table,
  * otherwise return NULL

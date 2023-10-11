9d1888d97b9d70ce9c44cc416ecbbe81daf1395d
aidan <aidanheydenrycht@gmail.com>
2023-10-11 20:15:32 +0200

    [Updated] Header file

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+/**
+ * hash_djb2 - Implementation of the djb2 algorithm.
+ * @str: String used to generate hash value
+ * Return: Hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}

c30db2f9dc0e82b773a74fb9726b18f85da78ff6
aidan <aidanheydenrycht@gmail.com>
2023-10-31 00:01:57 +0200

    [Fix] compilation fail

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,13 +13,31 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index = hash(key);
 
-	while (ht->table[index] != NULL)
+	while (ht->array[index] != NULL)
 	{
-		if (strcmp(ht->table[index]->key, key) == 0)
+		if (strcmp(ht->array[index]->key, key) == 0)
 		{
-		    return (ht->table[index]->value);
+		    return (ht->array[index]->value);
 		}
 		index = (index + 1) % TABLE_SIZE;
 	}
 	return (NULL);
+}
+
+/**
+ * hash - Convert a given key into an index within the hash table's array.
+ * @key: key looking for
+ * Return: index
+ */
+
+unsigned int hash(const char *key)
+{
+	unsigned int hash_value = 0;
+
+		while (*key)
+		{
+			hash_value = (hash_value * 31) + *key;
+			key++;
+		}
+	return (hash_value % TABLE_SIZE);
 }
